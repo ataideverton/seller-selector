c856adc3cf5d99b54ad206590a11ae803bb2b185
Everton Ataide <everton.ataide@ciashop.local>
2020-01-07 14:49:53 -0300

    Listando sellers

--- react/SellerSelector.tsx
+++ react/SellerSelector.tsx
@@ -1,7 +1,32 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import useProduct from 'vtex.product-context/useProduct'
 
-const SellerSelector: StorefrontFunctionComponent<any> = ({}) => {
-  return <Fragment></Fragment>
+const SellerSelector: StorefrontFunctionComponent<any> = ({ slug }) => {
+  const { selectedItem } = useProduct()
+  console.log(selectedItem)
+  console.log(slug)
+
+  if (selectedItem) {
+    return (
+      <div>
+        <tr>
+          <th>Loja</th>
+          <th>Preço Produto</th>
+          <th>Frete</th>
+          <th>Preço + Frete</th>
+          <th>-</th>
+        </tr>
+        {selectedItem.sellers.map((current: any, index: any) => (
+          <tr key={index}>
+            <td>{current.sellerName}</td>
+            <td>0.0</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        ))}
+      </div>
+    )
+  } else return <></>
 }
 
 //This is the schema form that will render the editable props on SiteEditor
