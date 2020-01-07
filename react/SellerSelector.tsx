f8d06bc912ec9fb5e81db4800bb177478a90ff7c
Everton Ataide <everton.ataide@ciashop.local>
2020-01-07 12:22:18 -0300

    Inicializando o app

---
+++ react/SellerSelector.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const SellerSelector: StorefrontFunctionComponent<any> = ({}) => {
+  return <Fragment></Fragment>
+}
+
+//This is the schema form that will render the editable props on SiteEditor
+SellerSelector.schema = {
+  title: 'editor.countdown.title',
+  description: 'editor.countdown.description',
+  type: 'object',
+  properties: {},
+}
+
+export default SellerSelector
