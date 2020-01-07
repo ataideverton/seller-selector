f642c218835e3ccd33a3eba5e8fd04f1a24aa404
gabrielbarross <gabriel.barros@vtex.com.br>
2020-01-07 15:43:29 -0300

    CSS Configuration

--- react/SellerSelector.tsx
+++ react/SellerSelector.tsx
@@ -1,28 +1,39 @@
 import React from 'react'
+// @ts-ignore
 import useProduct from 'vtex.product-context/useProduct'
+import { useCssHandles } from 'vtex.css-handles'
+
+const SELLERS_CSS_HANDLES = [
+  'sellersHeader',
+  'sellersInfoBox',
+  'buttonItemsPrice',
+]
 
 const SellerSelector: StorefrontFunctionComponent<any> = ({ slug }) => {
   const { selectedItem } = useProduct()
   console.log(selectedItem)
   console.log(slug)
+  const handles = useCssHandles(SELLERS_CSS_HANDLES)
 
   if (selectedItem) {
     return (
       <div>
-        <tr>
-          <th>Loja</th>
-          <th>Preço Produto</th>
-          <th>Frete</th>
-          <th>Preço + Frete</th>
-          <th>-</th>
-        </tr>
+        <div
+          className={`${handles.sellersHeader} flex br2 bg-muted-3 hover-bg-muted-3 active-bg-muted-3 c-on-muted-3 hover-c-on-muted-3 active-c-on-muted-3 dib mr3`}
+        >
+          <h5 className=" ph6 pv3 t-heading-5">Loja</h5>
+          <h5 className="ph6 pv3 t-heading-5">Preço Produto</h5>
+          <h5 className="ph6 pv3 t-heading-5">Frete</h5>
+          <h5 className="ph6 pv3 t-heading-5">Preço + Frete</h5>
+          <h5 className="ph6 pv3 t-heading-5">-</h5>
+        </div>
         {selectedItem.sellers.map((current: any, index: any) => (
-          <tr key={index}>
-            <td>{current.sellerName}</td>
-            <td>0.0</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
+          <div key={index} className={`${handles.sellersInfoBox}`}>
+            <p>{current.sellerName}</p>
+            <p>0.0</p>
+            <p>-</p>
+            <p>-</p>
+          </div>
         ))}
       </div>
     )
